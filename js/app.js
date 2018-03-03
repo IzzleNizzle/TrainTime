2e36876e99f13be24fd7795185a3d78976a652c0
IzzleNizzle <pehisaac@gmail.com>
2018-03-03 09:42:38 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,23 +1,64 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyB0DRdcrdiI-LCaIGrzQQSbKDKHqgTuDYY",
-    authDomain: "iz-traintime.firebaseapp.com",
-    databaseURL: "https://iz-traintime.firebaseio.com",
-    projectId: "iz-traintime",
-    storageBucket: "",
-    messagingSenderId: "361084541916"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyB0DRdcrdiI-LCaIGrzQQSbKDKHqgTuDYY",
+  authDomain: "iz-traintime.firebaseapp.com",
+  databaseURL: "https://iz-traintime.firebaseio.com",
+  projectId: "iz-traintime",
+  storageBucket: "",
+  messagingSenderId: "361084541916"
+};
+firebase.initializeApp(config);
 
 // Test connection with Database
 var db = firebase.database();
-db.ref().set({
-    highBidder: "TEST",
-    highPrice: "secondTest"
-  });
-// Test return values on the input type of time
-$("#submit-train").on("click", function() {
-    event.preventDefault();
-    console.log($("#train-time").val());
-    console.log(typeof $("#train-time").val());
+
+// Variables for all inputs
+var trainName,
+  destination,
+  trainTime;
+
+
+// on click function that submits the variables to the database using .ref().push() instead of .set
+$("#submit-train").on("click", function () {
+  event.preventDefault();
+
+  trainName = $("#train-name").val().trim();
+  destination = $("#destination").val().trim();
+  trainTime = $("#train-time").val().trim();
+
+  db.ref().push({
+    trainName: trainName,
+    destination: destination,
+    trainTime: trainTime,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  })
+
 })
+
+// create listener to catch changes in the database
+db.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+  // Variable to store response from database
+  var sv = snapshot.val();
+
+  // Building a new row to print information to the page
+  var row = $("<div class=row></div>");
+  var col1 = $("<div class=col-md-2></div>");
+  var col2 = $("<div class=col-md-2></div>");
+  var col3 = $("<div class=col-md-2></div>");
+  var col4 = $("<div class=col-md-2></div>");
+  var hr = $("<hr>");
+
+  col1.text(sv.trainName);
+  col2.text(sv.destination);
+  col3.text(sv.trainTime);
+
+  row.append(col1);
+  row.append(col2);
+  row.append(col3);
+  row.append(hr);
+
+  $("#employee-data").append(row);
+
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
